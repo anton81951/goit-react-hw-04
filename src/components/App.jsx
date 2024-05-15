b919456f2e7274716364eb61b601a23e9ae1c0a6
Anton81951 <antonbugaichuk@gmail.com>
2024-05-15 16:09:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
+
 import SearchBar from './SearchBar/SearchBar';
 import ErrorMessage from './ErrorMessage/ErrorMessage';
 import ImageGallery from './ImageGallery/ImageGallery';
 import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn';
 import Loader from './Loader/Loader';
-import { fetchPictures } from '../pictures-api';
 import ImageModal from './ImageModal/ImageModal';
+
+import { fetchPictures } from '../pictures-api';
+
 import styles from './App.module.css';
 
 const App = () => {
@@ -16,6 +19,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedImage, setSelectedImage] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showLoadMoreBtn, setShowLoadMoreBtn] = useState(false);
 
   useEffect(() => {
     if (searchTerm.trim() === '') {
@@ -28,6 +32,7 @@ const App = () => {
         setError(false);
         const response = await fetchPictures(searchTerm, page);
         setPictures((prevState) => [...prevState, ...response]);
+        setShowLoadMoreBtn(response.length > 0);
       } catch (error) {
         setError(true);
       } finally {
@@ -66,7 +71,7 @@ const App = () => {
         {isLoading && <Loader />}
         {error && <ErrorMessage />}
         {pictures.length > 0 && <ImageGallery pictures={pictures} onImageClick={openModal} />}
-        {pictures.length > 0 && !isLoading && <LoadMoreBtn onClick={handleLoadMore} />}
+        {showLoadMoreBtn && !isLoading && <LoadMoreBtn onClick={handleLoadMore} />}
         <ImageModal isOpen={isModalOpen} imageUrl={selectedImage} altText="Selected Image" closeModal={closeModal} />
       </div>
     </>
